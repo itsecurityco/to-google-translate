facb3ba479c95ea5852f71a90b97f0ae6fccd200
Christoforos Aslanov <christoforos@christoforos.com>
2019-03-16 19:57:53 +0200

    Added Translate Page to options page

--- options.js
+++ options.js
@@ -13,8 +13,11 @@ var storage = chrome.storage.local;
 var pageLang = document.querySelector('#pageLang');
 var userLang = document.querySelector('#userLang');
 var ttsLang = document.querySelector('#ttsLang');
+var TPpageLang = document.querySelector('#TPpageLang');
+var TPuserLang = document.querySelector('#TPuserLang');
 var enableTT = document.querySelector('#enableTT');
 var enableTTS = document.querySelector('#enableTTS');
+var enableTP = document.querySelector('#enableTP');
 
 function saveOptions(e) {
     e.preventDefault();
@@ -22,15 +25,21 @@ function saveOptions(e) {
         'pageLang': pageLang.value,
         'userLang': userLang.value,
         'ttsLang': ttsLang.value,
+        'TPpageLang': TPpageLang.value,
+        'TPuserLang': TPuserLang.value,
         'enableTT': enableTT.checked,
         'enableTTS': enableTTS.checked,
+        'enableTP': enableTP.checked,
         'translateURL': `https://${gtDomain}/?sl=${pageLang.value}&tl=${userLang.value}&text=`,
-        'ttsURL': `https://${gtDomain}/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&tl=${ttsLang.value}&q=`
+        'ttsURL': `https://${gtDomain}/translate_tts?ie=UTF-8&total=1&idx=0&client=tw-ob&tl=${ttsLang.value}&q=`,
+        'translatePageURL': `https://${gtDomain}/translate?sl=${TPpageLang.value}&tl=${TPuserLang.value}&u=`
     }, function () {
         updateContextMenuTitle('translate', 
             chrome.i18n.getMessage('contextMenuTitleTranslate', [pageLang.value, userLang.value]));
         updateContextMenuTitle('tts', 
             chrome.i18n.getMessage('contextMenuTitleTextToSpeech', ttsLang.value));
+        updateContextMenuTitle('translatePage',
+            chrome.i18n.getMessage('contextMenuTitleTranslatePage', [TPpageLang.value, TPuserLang.value]));
         showMessage(chrome.i18n.getMessage('optionsMessageSaved'));
 
         if (enableTT.checked == false) {
@@ -52,6 +61,16 @@ function saveOptions(e) {
                 contexts: ['selection']
             });
         }
+
+        if (enableTP.checked == false) {
+            removeContextMenu('translatePage');
+        } else {
+            chrome.contextMenus.create({
+                id: 'translatePage',
+                title: chrome.i18n.getMessage('contextMenuTitleTranslatePage', [TPpageLang.value, TPuserLang.value]),
+                contexts: ['all']
+            });
+        }
         
     });
 }
@@ -61,15 +80,21 @@ function loadOptions() {
         'pageLang': 'auto',
         'userLang': 'es',
         'ttsLang': 'en',
+        'TPpageLang': 'auto',
+        'TPuserLang': 'es',
         'enableTT': true,
         'enableTTS': true,
+        'enableTP': true,
         'gtDomain': getGoogleTranslatorDomain()
     }, function (items) {
         pageLang.value = items.pageLang;
         userLang.value = items.userLang;
         ttsLang.value = items.ttsLang;
+        TPpageLang.value = items.TPpageLang;
+        TPuserLang.value = items.TPuserLang;
         enableTT.checked = items.enableTT;
         enableTTS.checked = items.enableTTS;
+        enableTP.checked = items.enableTP;
         gtDomain = items.gtDomain;
     });
 }
