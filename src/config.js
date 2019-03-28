61f5113cb4e76a3fe220d45d02609f130da6ebea
Christoforos Aslanov <christoforos@christoforos.io>
2019-03-28 05:54:58 +0200

    fixes

--- src/config.js
+++ src/config.js
@@ -25,6 +25,7 @@ class Config {
                 'enableTT': true,
                 'enableTTS': true,
                 'enableTP': true,
+                'enableHotkeys': true,
                 'selectedDomain': 'global',
                 'openMode': 'modal',
                 'gtDomain': Config.getGoogleTranslatorDomain(),
