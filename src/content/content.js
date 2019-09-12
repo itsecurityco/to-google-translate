9713fba0db8d873d123765737edb65df667d2904
Christoforos Aslanov <christoforos@christoforos.io>
2019-09-12 07:53:59 +0300

    use the correct modal based on firefox version

--- src/content/content.js
+++ src/content/content.js
@@ -5,7 +5,13 @@ chrome.runtime.onMessage.addListener(function (message, sender, callback) {
 });
 
 function show_modal(url, fullscreen = false) {
-    let tgt_model = new Modal({url: url, fullscreen: fullscreen}, false);
+    let agentMatch = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+    let firefoxVersion = agentMatch ? parseInt(agentMatch[1]) : 0;
+
+    let tgt_model = firefoxVersion > 63
+        ? new Modal({url: url, fullscreen: fullscreen}, false)
+        : new ModalIframe({url: url, fullscreen: fullscreen}, false);
+
     tgt_model.build().then(() => {
         tgt_model.show();
     });
