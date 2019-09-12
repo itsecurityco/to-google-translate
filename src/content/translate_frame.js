59eeb2c14be33f595cbe8d36e7971611cab36200
Christoforos Aslanov <christoforos@christoforos.io>
2019-09-12 07:55:08 +0300

    hide google translate header by message

--- src/content/translate_frame.js
+++ src/content/translate_frame.js
@@ -1,26 +1,17 @@
 if (window.top !== window.self) {
-    if (document.querySelector("#contentframe > iframe")) {
-        let items = document.querySelectorAll("body > div:not(#contentframe)");
-        for (let elm of items) {
-            elm.style.display = "none";
-        }
-        document.querySelector("#contentframe").style.top = "0";
-    }
-
     window.addEventListener('message', function (event) {
-        if (event.data.action === "changeLang") {
-            if (location.hostname !== "translate.googleusercontent.com" && location.hostname !== "translate.googleusercontent.cn" && document.querySelector("iframe")) {
-                document.querySelector("iframe").contentWindow.postMessage({
-                    action: "changeLang",
-                    from: event.data.from,
-                    to: event.data.to
-                }, "*");
-            } else {
-                let url = new URL(location.href);
-                url.searchParams.set('sl', event.data.from);
-                url.searchParams.set('tl', event.data.to);
-                window.location.replace(url.toString());
+        let url = new URL(location.href);
+
+        if (event.data.action === "changeLang" && location.hostname === "translate.google.com" && url.searchParams.has("tl")) {
+            url.searchParams.set('sl', event.data.from);
+            url.searchParams.set('tl', event.data.to);
+            window.location.replace(url.toString());
+        } else if (event.data.action === "hideHeader" && document.querySelector("#contentframe > iframe") && location.hostname === "translate.google.com") {
+            let items = document.querySelectorAll("body > div:not(#contentframe)");
+            for (let elm of items) {
+                elm.style.display = "none";
             }
+            document.querySelector("#contentframe").style.top = "0";
         }
     }, false);
 }
